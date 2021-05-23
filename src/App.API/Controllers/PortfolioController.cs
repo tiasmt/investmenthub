47dafbe5d556cee8961e6d677a39e3738507f0a5
Matthias Fenech <matthias.fenech@BIT8.COM>
2021-05-23 22:54:58 +0200

    Update route for controller API methods

--- src/App.API/Controllers/PortfolioController.cs
+++ src/App.API/Controllers/PortfolioController.cs
@@ -2,25 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using App.API.Models;
+using App.API.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace App.API.Controllers
 {
-    [ApiController]
     [Route("[controller]")]
+    [ApiController]
     public class PortfolioController : ControllerBase
     {
         private readonly ILogger<PortfolioController> _logger;
+        private readonly IPortfolioService _portfolioService;
 
-        public PortfolioController(ILogger<PortfolioController> logger)
+        public PortfolioController(ILogger<PortfolioController> logger, IPortfolioService portfolioService)
         {
             _logger = logger;
+            _portfolioService = portfolioService;
         }
 
         [HttpGet]
         public async Task<IActionResult> Get()
         {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("deposit")]
+        public async Task<IActionResult> Deposit([FromBody] DepositReq deposit)
+        {
+            _portfolioService.Deposit(deposit.User, deposit.Amount);
+            return Ok();
+        }
+        [HttpPost]
+        [Route("withdraw")]
+        public async Task<IActionResult> Withdrawal([FromBody] WithdrawalReq withdrawal)
+        {
+            _portfolioService.Deposit(withdrawal.User, withdrawal.Amount);
+            return Ok();
+        }
+        [HttpPost]
+        [Route("buy")]
+        public async Task<IActionResult> BuyStock([FromBody] BuyStockReq buyRequest)
+        {
+            _portfolioService.BuyStock(buyRequest.User, buyRequest.Stock, buyRequest.Amount, buyRequest.Price);
+            return Ok();
+        }
+        [HttpPost]
+        [Route("sell")]
+        public async Task<IActionResult> SellStock([FromBody] SellStockReq sellRequest)
+        {
+            _portfolioService.SellStock(sellRequest.User, sellRequest.Stock, sellRequest.Amount, sellRequest.Price);
             return Ok();
         }
     }
